6ed9c8c9604235ecb6a8dffcd579fe8395a10fef
Demirichell <121028877+Demirichell@users.noreply.github.com>
2024-06-26 11:31:44 +0200

    setup + made nav

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+//import "./App.css";
+import Pages from "./Components/Pages/Pages";
+function App() {
+  return (
+    <>
+      <Pages></Pages>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,23 @@
+//import React from "react";
+
+import NavLinks from "./NavLinks";
+import UserLinks from "./UserLinks";
+
+const NavBar = () => {
+  return (
+    <div className="flex justify-between items-center border-b border-gray-100 w-full px-44 py-2">
+      <div className="text-3xl font-extrabold text-gray-900 dark:text-white">
+        <span className=" text-transparent bg-clip-text bg-gradient-to-r to-red-600 from-blue-600">
+          Game4You
+        </span>
+      </div>
+      <div className="flex justify-center item-center mx-auto">
+        <NavLinks></NavLinks>
+      </div>
+      <div>
+        <UserLinks></UserLinks>
+      </div>
+    </div>
+  );
+};
+export default NavBar;

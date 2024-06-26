764a3255e09bc82752564a651eeba7ac336149f0
Demirichell <121028877+Demirichell@users.noreply.github.com>
2024-06-26 18:41:05 +0200

    rightSide-styling-setup

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,5 +1,6 @@
 //import React from "react";
 import LeftSide from "../LeftSideBar/LeftSide";
+import RightSide from "../RightSideBar/RightSide";
 import NavBar from "../NavBar/NavBar";
 
 const Home = () => {
@@ -8,9 +9,13 @@ const Home = () => {
       <div className="top-0 z-10 w-full bg-white">
         <NavBar></NavBar>
       </div>
-      <div className="flex bg-gray-100"></div>
-      <div className="flex-auto w-[20%] fixed top-13">
-        <LeftSide></LeftSide>
+      <div className="flex bg-gray-100">
+        <div className="flex-auto w-[20%] fixed top-13">
+          <LeftSide></LeftSide>
+        </div>
+        <div className="flex-auto w-[20%] fixed right-0 top-13">
+          <RightSide></RightSide>
+        </div>
       </div>
     </div>
   );

f90321d8f3b23b92ce61329eada4fe43c8d2a85b
bleenie <boleenders01@gmail.com>
2024-06-30 22:53:05 +0200

    login setup

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -8,6 +8,7 @@ import {
     Input,
     Button,
   } from "@material-tailwind/react";
+  import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
@@ -19,31 +20,38 @@ const Login = () => {
           className="mb-4 grid h-28 place-items-center"
         >
           <Typography variant="h3" color="white">
-            Sign In
+            Sign in
           </Typography>
         </CardHeader>
         <CardBody className="flex flex-col gap-4">
-          <Input label="Email" size="lg" />
-          <Input label="Password" size="lg" />
-          <div className="-ml-2.5">
-          </div>
+            <form onSubmit="">
+                <div className="mb-2">
+                    <Input label="Email" size="lg" />
+                </div>
+                <div className="mt-4 mb-2"> 
+                    <Input label="Password" size="lg" /> 
+                </div>         
+                <Button variant="gradient" fullWidth className="mb-4" type="submit">
+                Sign in
+                </Button>
+          </form>
         </CardBody>
         <CardFooter className="pt-0">
-          <Button variant="gradient" fullWidth>
-            Sign In
+          <Button variant="gradient" fullWidth className="mb-4">
+            Sign in with Google
           </Button>
-          <Typography variant="small" className="mt-6 flex justify-center">
-            Don&apos;t have an account?
-            <Typography
-              as="a"
-              href="#signup"
-              variant="small"
-              color="blue-gray"
-              className="ml-1 font-bold"
-            >
-              Sign up
-            </Typography>
-          </Typography>
+          <Link>
+          <p className="ml-1 font-bold font-roboto text-sm text-blue-500 text-center">
+            Reset password
+            </p>
+            </Link>
+            <div className="mt-6 flex font-roboto text-base justify-center">
+              <Link>
+                <p className="ml-1 font-bold font-roboto text-sm text-blue-500 text-center">
+                    Register
+                </p>
+              </Link>
+            </div>          
         </CardFooter>
       </Card>
       </div>

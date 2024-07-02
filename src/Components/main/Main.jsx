955ccc2df03c072b0dae1fd50410b2e7f5e6a67e
Demirichell <121028877+Demirichell@users.noreply.github.com>
2024-07-02 10:29:17 +0200

    Update main-logic

--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -4,9 +4,41 @@ import hearticon from "../../assets/Images/hearticon.png";
 import controllericon from "../../assets/Images/controllericon.png";
 import addimg from "../../assets/Images/addimg.png";
 import { Button } from "@material-tailwind/react";
+import { useRef,useState,useContext } from "react";
+import {AuthContext} from "../AppContext";
+import {doc, setDoc, collection} from "firebase/firestore";
+import {db} from "../firebase/firebase";
+import { documentId } from "firebase/firestore/lite";
 
 const Main = () => {
   return (
+    const {User, userData} = useContext(AuthContext);
+    const [image, setImage] = useState(null);
+    const text = useRef("");
+
+    const collectionRef = collection(db, "posts");
+    const postRef = doc(collection(db, "posts"));
+    const document = postRef.id;
+    
+    const handleSubmitPost = async (e) =>{
+      try{
+        if(text.current.value !== ""){
+          await setDoc(postRef,{
+            documentId: document,
+            uid: user?.uid || userData?.uid,
+          logo: user?.photoURL,
+          name: user?.displayName || userData?.name,
+          email: user?.email || userData?.email,
+          text: text.current.value,
+          image: image,
+          timestamp: serverTimestamp(),
+          });
+        }
+      }catch(err){
+        alert(err.message);
+        console.log(err.message)
+      }
+    };
     <div className="flex flex-col items-center">
       <div className="flex flex-col py-4 w-full bg-white rounded-3xl shadow-lg">
         <div className="flex items-center border-b-2 border-gray-300 pb-4 pl-4 w-full">
@@ -22,8 +54,9 @@ const Main = () => {
                 <input
                   type="text"
                   name="text"
-                  placeholder="Whats on your mind?"
+                  placeholder={`whats on your mind? ${user?.displayName?.split("")[0] ||userData?.name.charAt(0).toUpperCase() + userData?.name.slice(1)}`}
                   className="outline-none w-full bg-white rounded-md"
+                  ref={text}
                 />
               </div>
               <div className="mx-4"> {/* TO:DO previewImage*/}</div>
